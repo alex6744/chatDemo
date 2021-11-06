ce84aba608abd6b69d6bce5ab51271d8ea96520c
alex6744 <alexngai6744@gmail.com>
2021-11-06 06:36:56 -0700

    add features

--- app/src/main/java/com/example/firebasedemo/StickMessage.java
+++ app/src/main/java/com/example/firebasedemo/StickMessage.java
@@ -9,16 +9,41 @@ import java.time.LocalDateTime;
 public class StickMessage {
     private String time;
     private String text;
-    private String image;
+   // private String image;
+    private boolean isImage;
 
     public StickMessage() {
     }
 
-    public StickMessage(String time, String text) {
+    public StickMessage(String time, String text,boolean isImage) {
         this.time = time;
+        this.text = text;
+        this.isImage=isImage;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
         this.text = text;
     }
 
+    public boolean getisImage() {
+        return isImage;
+    }
+
+    public void setisImage(boolean text) {
+        isImage = text;
+    }
 
     @Override
     public String toString() {
