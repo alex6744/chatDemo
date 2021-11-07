ebf0ebe41ca031edfada645963efe9858f402ffe
alex6744 <alexngai6744@gmail.com>
2021-11-07 09:57:33 -0800

    create recyclerView for image class

---
+++ app/src/main/java/com/example/firebasedemo/chatComponent/image/ImageHolder.java
@@ -0,0 +1,45 @@
+package com.example.firebasedemo.chatComponent.image;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.firebasedemo.R;
+import com.example.firebasedemo.chatComponent.ItemClickListener;
+
+public class ImageHolder extends RecyclerView.ViewHolder {
+
+
+
+
+    public ImageView image;
+
+    public ImageHolder(View itemView, final ItemClickListener listener) {
+        super(itemView);
+
+
+        image=itemView.findViewById(R.id.emoji_image);
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    int position = getLayoutPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+
+                        listener.onItemClick(position);
+                    }
+                }
+
+
+
+            }
+        });
+
+
+    }
+
+}
